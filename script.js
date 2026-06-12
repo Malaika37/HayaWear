98c6eed34a64d96b8d4a456fcc706ec86af88055
Malaika37 <minhajashfaq40@gmail.com>
2026-06-12 23:24:25 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -18,21 +18,10 @@ const nav = document.getElementById('navbar');
 let MainImg = document.getElementById("MainImage"); 
 let smallimg = document.getElementsByClassName("small-img");
 
-smallimg[0].onclick = function(){
-MainImg.src = smallimg[0].src;
-}
-
-smallimg[1].onclick = function(){
-MainImg.src = smallimg[1].src;
-}
-
-smallimg[2].onclick = function(){
-MainImg.src = smallimg[2].src;
-}
-
-smallimg[3].onclick = function(){
-MainImg.src = smallimg[3].src;
-}
-
+Array.from(smallimg).forEach((img) => {
+    img.onclick = function() {
+        MainImg.src = img.src;
+    };
+});
  
  
